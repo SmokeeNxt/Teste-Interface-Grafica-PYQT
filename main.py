fcd83e96c6c82e12a68a94aa8bca1520ddcc894c
Jhonys Martins <49736842+SmokeeNxt@users.noreply.github.com>
2023-04-04 18:57:04 -0300

    Arquivos do codigo!
    Subindo os arquivos pela primeira vez no repositorio

---
+++ main.py
@@ -0,0 +1,41 @@
+import sys
+from PyQt5.uic import loadUi
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QToolTip, QLineEdit
+from PyQt5 import QtGui
+
+
+  #Gerando a Classe (JANELA MAIN)
+###################################
+class UI(QMainWindow):
+    def __init__(self):
+        super(UI, self).__init__()
+        loadUi("Conv.ui", self)
+        self.titulo = "Arthur"
+        self.pushButton_Converter.clicked.connect(self.Conversao)
+        self.Recept_User_Money
+        self.setWindowTitle(self.titulo)
+ #################################
+    def Conversao(self):
+        conteudo = self.Recept_User_Money.text()
+        isnum = conteudo.isnumeric()
+        if isnum == False:
+           self.label_Result.setText("Digite somente números!")
+        else:
+            dolar = 5.06 #no dia em que eu fiz, o dolar estava neste valor =)
+            conta = float(conteudo) / dolar
+            self.label_Result.setText(f"Resultado da Conversão: U${conta:.2f}")
+        #print(conteudo)
+
+
+
+
+
+    #Janela do Programa#
+################################
+app = QApplication(sys.argv)
+window = UI()
+window.show()
+sys.exit(app.exec_())
+################################
+
+
